83c51685f6180182a3aa70702e77c5cc49b428fe
sai teja <sattirajusaiteja@gmail>
2025-05-21 21:55:37 +0530

    Fix: Removed unused imports to fix Vercel build error

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,5 @@
-import Image from "next/image";
-import { MorphingText } from "@/components/magicui/morphing-text"; // Named import
-import { ReNavbar } from "@/components/magicui/resizable-navbar"; // Named import
-import { Vortex } from "@/components/magicui/vortex"; // Named import
+import { ReNavbar } from "@/components/magicui/resizable-navbar"; 
+import { Vortex } from "@/components/magicui/vortex"; 
 import { BackgroundBeams } from "@/components/magicui/background-beams";
 import { Clock } from "lucide-react";
 import { HyperText } from "@/components/magicui/hypertext";
